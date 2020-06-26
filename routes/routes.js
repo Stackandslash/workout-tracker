930618d350b61f5d847e4b0353e87e5ee9e85710
Haven <stackandslash@gmail.com>
2020-06-25 19:23:36 -0700

    Routing, model test, commenting discovered api calls

--- routes/routes.js
+++ routes/routes.js
@@ -4,32 +4,41 @@ const path = require("path");
 
 module.exports = function (app) {
   // Route to post our form submission to mongoDB via mongoose
-  app.post("/submit", ({ body }, res) => {
-    // Create a new user using req.body
-    let user = new User(body);
+  app.get("/api/workouts", (req, res) => {
+    console.log("We tried to get!");
+    // Workouts calls ask for the content, then process it. They appear to turn it back into a JSON in the processing, 
 
-    User.create(user)
-      .then((dbUser) => {
-        // If saved successfully, send the the new User document to the client
-        res.json(dbUser);
-      })
-      .catch((err) => {
-        // If an error occurs, send the error to the client
-        res.json(err);
-      });
+    //let user = new User(body);
+  });
+  
+  app.post("/api/workouts", (req, res) => {
+    console.log("We tried to post!")
+    // let user = new User(body);
   });
 
+  app.put("/api/workouts", (req, res) => {
+    console.log("We tried to put!")
+    // let user = new User(body);
+
+    // Chart.create(stuff)
+    //   .then((thing) => {
+    //     res.json(thing);
+    //   })
+    //   .catch((err) => {
+    //   });
+  });
+
+
+
+  //HTML Routing here.
   app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 
   app.get("/stats", (req, res) => {
-    // If the user already has an account send them to the members page
     res.sendFile(path.join(__dirname, "../public/stats.html"));
   });
 
-  // Here we've add our isAuthenticated middleware to this route.
-  // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/exercise", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/exercise.html"));
   });
